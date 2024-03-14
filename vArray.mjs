69852e594a416b1181812c4e3512605c20e4677d
anamboijohn <anamboijohn@gmail.com>
2024-03-14 15:10:45 +0000

    =added a version of vArray that can visualise both negative and positive numbers

--- vArray.mjs
+++ vArray.mjs
@@ -4,7 +4,7 @@
 
 //NOTE if you are downloading this alone, download the data.mjs file for testing data, or use ur own data for testing
 
-import { data20, data20Sorted } from "./data.mjs";
+import { data20, data20Sorted, fish } from "./data.mjs";
 export default function arrayVisualizer(arr) {
     let maximum = Math.max(...arr)-1;
     // console.log(maximum, " Maximum");
@@ -24,5 +24,60 @@ export default function arrayVisualizer(arr) {
     return "Done";
 }
 
-console.log(arrayVisualizer(data20));
-console.log(arrayVisualizer(data20Sorted));
+// console.log(arrayVisualizer(data20));
+// console.log(arrayVisualizer(data20Sorted));
+
+function arrayVisualizer1(arr) {
+    let maximum = Math.max(...arr);
+	let minimum = Math.min(...arr);
+	function visualize(max) {
+		if(minimum<0){
+			if (max < minimum+1) return;
+		}else{
+			if (max < 0) return;
+		}
+		let block = "";
+		for (let el of arr) {			
+				max > 1? (el >= max )? block += "* " : block += "  ":
+				max < 1? (el + 2 <= max) ? block += "* " : block += "  ":block += "* ";
+		}
+		console.log(block);
+
+		visualize(max - 1);
+	}
+
+    visualize(maximum);
+
+    return "Done";
+}
+// console.log(arrayVisualizer1([4, 4, 4, 4]))
+
+function arrayVisualizer2(arr) {
+    let maximum = Math.max(...arr);
+	let minimum = Math.min(...arr);
+	function visualize(max) {
+		
+		if(minimum<0){
+			if (max < minimum) return;
+		}else{
+			if (max < 0 ) return;
+		}
+		let block = "";
+		for (let el of arr) {			
+				max > 0? (el >= max )? block += "# " : block += "  ":
+				max < 0? (el  <= max) ? block += "# " : block += "  ":block += " ";
+				
+				// console.log(el)
+				// console.log(block);
+		}
+		console.log(block);
+
+		visualize(max - 1);
+	}
+
+    visualize(maximum);
+
+    return "Done";
+}
+//console.log(arrayVisualizer(fish))
+console.log(arrayVisualizer2([1, 2, 3, 4,5,6,7,6,5,4,3,2,1,2,3,4,5,6,5,4,3,2,1,2,3,4, 5,4,3,2,1,2,3,4,3,2,1,2,3,2,1, -1, -2, -3, -4,-5,-6,-7,-6,-5,-4,-3,-2,-1,-2,-3,-4,-5,-6,-5,-4,-3,-2,-1,-2,-3,-4,-5,-4,-3,-2,-1,-2,-3,-4,-3,-2,-1,-2,-3,-2,-1]))
