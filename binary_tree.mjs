b1ccd692091f0865fe9023f1ec48913c8b44eaa9
anamboijohn <anamboijohn@gmail.com>
2024-04-03 23:46:54 +0000

    mannually create tree

--- binary_tree.mjs
+++ binary_tree.mjs
@@ -7,3 +7,13 @@ class Node{
 
 }
 
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+const e = new Node('e')
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
