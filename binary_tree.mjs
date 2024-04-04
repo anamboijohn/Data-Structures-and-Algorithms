2a5050962c38980eed4b32eda995d951ea0d42bd
anamboijohn <anamboijohn@gmail.com>
2024-04-04 00:13:55 +0000

    add iterative solution for depth first traversal

--- binary_tree.mjs
+++ binary_tree.mjs
@@ -17,3 +17,22 @@ a.left = b;
 a.right = c;
 b.left = d;
 b.right = e;
+
+//Depth first traversal
+const depthFirstValues = (root) =>{
+    if(!(root instanceof Node)) return []
+    let result =[]
+    const stack = [root]
+    while(stack.length>0){
+        let current = stack.pop()
+        result.push(current.val)
+
+        //add the node children to the stack
+        current.right?stack.push(current.right) : null 
+        current.left?stack.push(current.left) : null 
+    }
+    return result
+}
+
+console.log(depthFirstValues(a))
+
